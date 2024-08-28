9f7d8e586ef61b99d4e245e33a47d71b46a4bb17
Carolina Sarnevesht <carops67@gmail.com>
2024-08-28 00:03:38 -0700

    small fix

--- src/firebase/addDocument.tsx
+++ src/firebase/addDocument.tsx
@@ -1,5 +1,5 @@
-import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore';
-import { db } from './config';
+import { collection, doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "./config";
 const addDocument = (collectionName: string, documentObj: any, id: string) => {
   const docRef = doc(collection(db, collectionName), id);
   return setDoc(docRef, {
