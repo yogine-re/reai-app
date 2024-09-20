a771da8acb9eb3df4ba2960b24971b76efcd770e
Carolina Sarnevesht <carops67@gmail.com>
2024-09-20 10:38:55 -0700

    fixes

--- src/googledrive/renameFolder.ts
+++ src/googledrive/renameFolder.ts
@@ -16,19 +16,19 @@ export async function renameFolder(googleApi: any, folderName: string, newfolder
 
         if (folders.length > 0) {
             const folderId = folders[0].id; // Return the ID of the first found folder
-            googleApi.client.drive.files.update({
+            // Update the folder's name
+            const updateResponse = await googleApi.client.drive.files.update({
                 fileId: folderId,
-                resource: requestBody,
-            }).then((response: any) => {
-                console.log('Folder updated successfully:', response.result);
-            }).catch((error: any) => {
-                console.error('Error updating folder:', error);
+                requestBody,
+                fields: 'id, name',
             });
+
+            console.log('Folder updated successfully:', updateResponse.result);
         } else {
             console.log('Folder not found:', folderName);
         }
     } catch (error) {
-        console.error('Error updating folder}:', error);
+        console.error('Error updating folder:', error);
     }
     
 }
